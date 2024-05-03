b070847e924490df41eaa0b2f6c2487a71227ec2
Abdala Atia <abdalaalaa17@gmail.com>
2024-05-03 21:23:34 +0300

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,55 @@
+import java.sql.Date;
+import java.util.Random;
+
+class Book {
+    private static final Random random = new Random();
+    private final String id;
+    private final String name;
+    private final String category;
+    private boolean borrowed;
+    private int borrowingPeriod;
+    private Date borrowingDate;
+
+    public Book(String name, String category) {
+        this.id = String.format("%04d", random.nextInt(10000)); // Generates a random number between 0 and 9999 as ID, with leading zeros
+        this.name = name;
+        this.category = category;
+        this.borrowed = false;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public boolean isBorrowed() {
+        return borrowed;
+    }
+
+    public void setBorrowed(boolean borrowed) {
+        this.borrowed = borrowed;
+    }
+
+    public int getBorrowingPeriod() {
+        return borrowingPeriod;
+    }
+
+    public void setBorrowingPeriod(int borrowingPeriod) {
+        this.borrowingPeriod = borrowingPeriod;
+    }
+
+    public Date getBorrowingDate() {
+        return borrowingDate;
+    }
+
+    public void setBorrowingDate(Date borrowingDate) {
+        this.borrowingDate = borrowingDate;
+    }
+}
